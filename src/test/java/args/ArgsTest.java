bbb58290ecd4f6b62fb13e1085f2694359241d61
GeorgeWu <stupidfive@gmail.com>
2020-05-11 23:37:21 +0800

    Refactor tests

--- src/test/java/args/ArgsTest.java
+++ src/test/java/args/ArgsTest.java
@@ -1,15 +1,23 @@
 package args;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArgsTest {
+    ArgumentParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new ArgumentParser();
+    }
+
     @Test
     void testBooleanFlag() {
-        ArgumentParser parser = new ArgumentParser();
         Argument arg = new Argument("-l", ArgumentType.BOOL);
         parser.addArgument(arg);
+
         Args args = parser.parse("-l");
 
         assertTrue(args.getBoolean("-l"));
@@ -17,9 +25,9 @@ public class ArgsTest {
 
     @Test
     void testBooleanFlagFalse() {
-        ArgumentParser parser = new ArgumentParser();
         Argument arg = new Argument("-l", ArgumentType.BOOL);
         parser.addArgument(arg);
+
         Args args = parser.parse("");
 
         assertFalse(args.getBoolean("-l"));
@@ -27,9 +35,9 @@ public class ArgsTest {
 
     @Test
     void testBooleanFlagTwice() {
-        ArgumentParser parser = new ArgumentParser();
         parser.addArgument(new Argument("-l", ArgumentType.BOOL));
         parser.addArgument(new Argument("-v", ArgumentType.BOOL));
+
         Args args = parser.parse("-l -v");
 
         assertTrue(args.getBoolean("-l"));
