31f39faa469eb68f8403f8a7fed5c601dd86646a
GeorgeWu <stupidfive@gmail.com>
2020-05-11 22:57:05 +0800

    Initial commit

---
+++ src/test/java/args/ArgsTest.java
@@ -0,0 +1,12 @@
+package args;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ArgsTest {
+    @Test
+    public void name() {
+        assertFalse(true);
+    }
+}
